2d6bf3765daa4f5779ec6e58bd1d9a646307ba36
YaakovG <ygoldenbergyo@gmail.com>
2024-04-21 13:21:15 -0400

    did it

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+    public class Program
+    {
+     
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number");
+            int num = int.Parse(Console.ReadLine());
+              var num1 =  FizzBuzz(num);
+            Console.WriteLine(num1);
+
+        }
+
+        public static string FizzBuzz( int num)
+        {
+            var answer = "";
+
+            if (num % 3 == 0)
+            {   
+                answer = "Fizz";
+            }
+              
+            if (num % 5 == 0)
+            {
+
+                answer += "Buzz";
+            }
+           
+            return answer;
+        }
+
+    }
+
+
+
+}
+
+
+
+
